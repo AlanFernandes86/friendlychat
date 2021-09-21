80ad18fc62284ff66c88589362cdbef725f3dfaf
Alan Fernandes <josefernandes.alan@gmail.com>
2021-09-21 19:47:41 -0300

    implementado firebase storage

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.kt
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.google.firebase.udacity.friendlychat
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.InputFilter
@@ -9,6 +11,8 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.*
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
@@ -18,7 +22,11 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mMessageListView: ListView
@@ -33,11 +41,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mChildEventListener: ChildEventListener
     private lateinit var mFirebaseAuth: FirebaseAuth
     private lateinit var mAuthStateListener: FirebaseAuth.AuthStateListener
+    private lateinit var mFirebaseStorage: FirebaseStorage
+    private lateinit var mChatPhotoStorageReference: StorageReference
 
     private val signInLauncher = registerForActivityResult(
             FirebaseAuthUIActivityResultContract()
     ) { res ->
         this.onSignInResult(res)
+    }
+
+    private val getPhotoLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+    ) { res ->
+        this.onGetPhotoResult(res)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +64,9 @@ class MainActivity : AppCompatActivity() {
         //Initialize Firebase components
         mFirebaseDatabase = Firebase.database
         mFirebaseAuth = Firebase.auth
+        mFirebaseStorage = Firebase.storage
         mMessagesDatabaseReference = mFirebaseDatabase.reference.child("messages")
+        mChatPhotoStorageReference = mFirebaseStorage.reference.child("chat_photos")
 
         // Initialize references to views
         mProgressBar = findViewById<View>(R.id.progressBar) as ProgressBar
@@ -67,7 +85,10 @@ class MainActivity : AppCompatActivity() {
 
         // ImagePickerButton shows an image picker to upload a image for a message
         mPhotoPickerButton.setOnClickListener {
-            // TODO: Fire an intent to show an image picker
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/jpeg"
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+            getPhotoLauncher.launch(intent)
         }
 
         // Enable Send button when there's text to send
@@ -91,11 +112,10 @@ class MainActivity : AppCompatActivity() {
 
         mAuthStateListener = (FirebaseAuth.AuthStateListener {
             val firebaseUser = it.currentUser
-            if(firebaseUser != null){
+            if (firebaseUser != null) {
                 // User is signed in
                 onSignedInInitialize(firebaseUser.displayName ?: ANONYMOUS)
-            }
-            else{
+            } else {
                 // User is signed out
                 onSignedOutCleanup()
                 createSignInIntent()
@@ -120,7 +140,8 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onChildRemoved(snapshot: DataSnapshot) {
-                //TODO("Not yet implemented")
+                val friendlyMessage = snapshot.getValue(FriendlyMessage::class.java)
+                mMessageAdapter.remove(friendlyMessage)
             }
 
             override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
@@ -141,7 +162,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun detachDatabaseReadListener() {
-        if(::mChildEventListener.isInitialized){
+        if (::mChildEventListener.isInitialized) {
             mMessagesDatabaseReference.removeEventListener(mChildEventListener)
         }
     }
@@ -154,7 +175,7 @@ class MainActivity : AppCompatActivity() {
                 AuthUI.IdpConfig.GoogleBuilder().build(),
                 //AuthUI.IdpConfig.FacebookBuilder().build(),
                 //AuthUI.IdpConfig.TwitterBuilder().build()
-                )
+        )
 
         // Create and launch sign-in intent
         val signInIntent = AuthUI.getInstance()
@@ -168,20 +189,52 @@ class MainActivity : AppCompatActivity() {
 
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
         val response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
-            Toast.makeText(this, "You're now signed in. Welcome to FriendlyChat.", Toast.LENGTH_SHORT).show()
-            // ...
+        when (result.resultCode) {
+            RESULT_OK -> {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(this, "You're now signed in. Welcome to FriendlyChat.", Toast.LENGTH_SHORT).show()
+                // ...
+            }
+            RESULT_CANCELED -> {
+                finish()
+            }
+            else -> {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
         }
-        else if(result.resultCode == RESULT_CANCELED) {
-            finish()
-        }
-        else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
+    }
+
+    private fun onGetPhotoResult(result: ActivityResult) {
+        when (result.resultCode) {
+            RESULT_OK -> {
+                val selectedPhotoUri = result.data?.data as Uri
+                val photoRef = mChatPhotoStorageReference.child(selectedPhotoUri.lastPathSegment as String)
+                val uploadTask = photoRef.putFile(selectedPhotoUri)
+
+                uploadTask.continueWithTask { task ->
+                    if (!task.isSuccessful) {
+                        task.exception?.let {
+                            throw it
+                        }
+                    }
+                    photoRef.downloadUrl
+                }.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val downloadUri = task.result as Uri
+                        val friendlyMessage = FriendlyMessage(null, mUsername, downloadUri.toString())
+                        mMessagesDatabaseReference.push().setValue(friendlyMessage)
+                    }
+                }
+            }
+            RESULT_CANCELED -> {
+                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+            }
         }
     }
 
@@ -192,7 +245,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId){
+        return when (item.itemId) {
             R.id.sign_out_menu -> {
                 AuthUI.getInstance().signOut(this)
                 true
@@ -214,7 +267,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val TAG = "MainActivity"
+        const val TAG = "MainActivity"
         const val ANONYMOUS = "anonymous"
         const val DEFAULT_MSG_LENGTH_LIMIT = 1000
     }
